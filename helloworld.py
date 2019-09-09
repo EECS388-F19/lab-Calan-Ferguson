e799cb4be09f8e4ecc810e3bf76bfeb90a52f7e4
cefergus <cefergus@umich.edu>
2019-09-09 15:36:55 -0400

    Lab done

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+print("Calan Ferguson")
+x = random.randint(0, 100)
+y =random.randint(0, 100)
+print(x)
+print(y)
+sum = x + y
+print("Sum = " + str(sum))
+avg = (x + y) / 2
+print("Average = " + str(avg))
